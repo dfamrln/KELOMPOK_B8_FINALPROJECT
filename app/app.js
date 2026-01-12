6e5dcab229579275fd05e16425988d5707cc0b52
fadilsyy <fadilasytar@gmail.com>
2026-01-12 22:49:37 +0700

    tambahan pada app

--- app/app.js
+++ app/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const todoRoutes = require('./app/routes/todoRoutes');
+const userRoutes = require('./app/routes/index');   // const index
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api', todoRoutes);
+app.use('/api/todos', todoRoutes);  // api todos
+app.use('/api/users', userRoutes);  // api users
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API is running' });
+});     // msg running
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});     // msg error
 
 const PORT = 3000;
 app.listen(PORT, () => {
